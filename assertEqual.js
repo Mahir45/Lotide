b70b7f430f83109d9f7c14661b449d789028199d
Mahir Ahmed <mahir.ahmed56@hotmail.com>
2021-10-25 19:51:29 -0400

    Emojis added

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log ("✅✅Assertion Passed:");
+  }
+  else {
+    console.log("🛑🛑Assertion Failed");
+  }
+  
+};
+assertEqual("6", "2");
+  assertEqual(1, 1);
