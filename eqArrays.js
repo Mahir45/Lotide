1f0e773e390d7adbd380982eb693944e3e1f8023
Mahir Ahmed <mahir.ahmed56@hotmail.com>
2021-10-27 13:54:23 -0400

    Create function eqArray

---
+++ eqArrays.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log ("✅✅Assertion Passed:");
+  }
+  else {
+    console.log("🛑🛑Assertion Failed");
+  }
+  
+};
+assertEqual(["1", "2", "3"], ["1", "2", "3"]);
+  assertEqual(["1", "2", "3"] ["1", "2", "3"]);
