d2e14b07d2963ca4e4fd54b01c18fe97f5b72591
Mahir Ahmed <mahir.ahmed56@hotmail.com>
2021-11-01 22:21:32 -0400

    refactored test

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,12 @@
-const head = require('../head');
-const assertEqual = require('../assertEqual');
-let people = ["Location", "wizkid", "wonder"]
-let wizard = [4, 5, 6]
-assertEqual((head(wizard)), 4);
-assertEqual((head(people)), 'Location');
-assertEqual((head([])), undefined);
+
+const assert = require('chai').assert;
+const head   = require('../head');
+const { it } = require('mocha');
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+});
+it("returns '5' for ['5']", () => {
+  assert.strictEqual(head(['5']), '5'); 
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,4 +1,16 @@
 const tail = require('../tail');
+const { it } = require('mocha');
+const { assert } = require('chai');
 
-let people = ["you", "are", "goofy",]
-tail (people)
+// let people = ["you", "are", "goofy",]
+// tail (people)
+
+describe("#tail", () => {
+  it("returns 3 for length of [1, 2, 3]", () => {
+    assert.strictEqual((tail([1, 2, 3])).length, 2);
+  });
+  it("returns '6' for ['5', '6']", () => {
+    assert.strictEqual((tail(['6', '5'])).length, 1); 
+  }); 
+  
+});
