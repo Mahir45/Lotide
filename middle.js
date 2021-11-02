5ae21cc579518afb03b534b05ca1089979c9c02b
Mahir Ahmed <mahir.ahmed56@hotmail.com>
2021-11-01 23:54:28 -0400

    new

--- middle.js
+++ middle.js
@@ -1,7 +1,9 @@
 
 const middle = function(array) {
   if (array.length % 2 === 0) {
-
+    if (array.length <= 2) {
+      return [];
+    }
     let midEven = array[Math.floor((array.length - 1)/ 2)];
     let midNum = array[Math.floor((array.length) / 2)];
     return [midEven, midNum]
@@ -11,6 +13,8 @@ const middle = function(array) {
     return midNum
   }
 }
+let arr1 = [1,6]
+console.log(middle(arr1))
 
 
 module.exports = middle
